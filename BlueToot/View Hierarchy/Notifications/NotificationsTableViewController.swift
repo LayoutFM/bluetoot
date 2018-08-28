6865e5a67d4431095e1db9f16055fbbef8ec6be2
Rafael Conde <rafahari@me.com>
2018-08-28 07:45:09 +0100

    Clean up from rebase

--- BlueToot/View Hierarchy/Notifications/NotificationsTableViewController.swift
+++ BlueToot/View Hierarchy/Notifications/NotificationsTableViewController.swift
@@ -10,18 +10,11 @@ import UIKit
 
 class NotificationsTableViewController: TableViewControllerWithDataAdapter {
 
-  init() {
-    super.init(coder: .plain)
-    title = "Notifications"
-    tabBarItem = UITabBarItem(title: "Notifications", image: UIImage(named: "notifications"), tag: 0)
-  }
-
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    title = "Notifications"
+    tabBarItem = UITabBarItem(title: "Notifications", image: UIImage(named: "notifications"), tag: 0)
     
     // Set up pull-to-refresh
     let refreshControl = UIRefreshControl()
